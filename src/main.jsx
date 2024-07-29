898234f0aac94501c45f1d5457540f591cc1e0a7
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2024-07-29 02:48:14 -0700

    idk whats wrong, setup worked with non-vite react

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router basename='/SimpleSmashScorebugs'>
+  <Router >
       <App />
   </Router>
 );
