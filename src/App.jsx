34afa60d801d9b507b31e058a81c3938cab8c3e9
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2024-07-29 02:08:57 -0700

    switching to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {HashRouter as Router, Route, Routes } from "react-router-dom";
 import Controller from "./Pages/scorecontroller";
 import Landing from "./Pages/landing";
 import Comms from "./Components/NTZ/comms";
