6be4231a8f5673329b7f7355da72b1fd5a59b286
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2024-09-25 21:59:01 -0700

    updating links

--- src/Components/oauthConfig.jsx
+++ src/Components/oauthConfig.jsx
@@ -1,6 +1,6 @@
 const oauthConfig = {
     client_id:  '175',
-    redirect_uri: 'https://hunter-mcmahon85.github.io/SimpleSmashScorebugs/#/TokenAuth', 
+    redirect_uri: 'https://www.simplesmashscorebugs.com/#/TokenAuth', 
     auth_endpoint: 'http://api.start.gg/oauth/authorize',
     token_endpoint: 'http://api.start.gg/oauth/access_token',
     response_type: 'code', 
