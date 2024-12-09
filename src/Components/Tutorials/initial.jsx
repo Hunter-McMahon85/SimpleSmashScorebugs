712ccda79d5898e3d1b7acc4abbc5d27c4698fd9
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2024-12-08 23:52:18 -0800

    documentation and visuals revision. added youtube Tutorials

--- src/Components/Tutorials/initial.jsx
+++ src/Components/Tutorials/initial.jsx
@@ -9,28 +9,45 @@ const Initial = () => {
                 <h3>
                     Initial Setup Instructions
                 </h3>
+                <iframe width="600" height="330" src="https://www.youtube.com/embed/ICGO1dxtRzo?si=ejdblGuucgBPk8Ab" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 <ol>
                     <li>Copy the link to the Scorebugs Page: https://www.simplesmashscorebugs.com/#/scorebugs</li>
                     <li>Open Open Broadcasting Software Studio (OBS)</li>
                     <li>From the OBS topbar, click on "Docks" then on "Custom Browser Docks"</li>
-                    <li>In the "Custom Browser Docks" menu that pops up, put an arbitrary name in the dock name section and past the link to the scorebugs page in the "URL" field and click apply</li>
-                    <li>At this point, the website will appear in an OBS window. This window can be dragged around or placed inside the OBS dashboard like any other dock, place it in your OBS as you please</li>
+                    <li>In the "Custom Browser Docks" menu, put an name in the dock name section and paste the link to the scorebugs page in the "URL" field and click apply</li>
+                    <li>the website will appear in an OBS window. This window can be dragged around or placed inside the OBS dashboard like any other dock, place it in your OBS as you please</li>
                     <li>Close the Custom Browser Docks menu, we do not need it anymore</li>
-                    <li>Create a new Browser source in the sources panel in OBS</li>
                     <li>
-                        In the properties tab, do the following before clicking ok:
+                        Now Add a new Browser Source to your stream in the OBS sources window/tab.
+                    </li>
+                    <li>
+                        When prompted, Click "Create New", Enter "Scorebug" into the text box and Click "ok"
+                    </li>
+                    <li>
+                        in the URL field, enter the link: https://www.simplesmashscorebugs.com/#/scoreboard<br />
+                        Then, ensure the following before clicking ok:
                         <ul>
-                            <li>set the URL field to be https://www.simplesmashscorebugs.com/#/scoreboard</li>
-                            <li>set the width to 1920px and height to 1080px. This is Smash ultimates native resolution, the scorebugs are hard set to match this resolution as your stream output should match this resolution</li>
-                            <li>Delete the Custom CSS field</li>
+                            <li>Width is Set to 1920</li>
+                            <li>Height is Set to 1080</li>
+                            <li>The custom CSS field is empty</li>
                         </ul>
                     </li>
-                    <li>In the stream preview Make sure the Browser source we just added takes up the entire preview space. If not then you either improperly sized the browser soure or your stream output is not set to 1080p</li>
-                    <li>The scoreboard is now completely setup and you are ready to use the overlay</li>
-                    <li>To use and overlay, click on a scorebug theme from the "Scorebug Selection" page you added as a Custom Browser Dock</li>
-                    <li>This will open a controller page that you will use to control the overlay. Once you see this page, Refresh the browser source in the stream sources panel</li>
-                    <li> Upon refresh, you should now see the singles version of the scorebug theme appear you are now ready to to use your overlay</li>
                 </ol>
+                <h4>
+                    Final Checks
+                </h4>
+                <p>The scorebug utility should now be added to your stream. In case it looks funky or nothing appears. Consider the following:</p>
+                <ul>
+                    <li>In the stream preview, ensure your browser source takes up the entire preview space. If not, ensure it is sized to be 1080p (1920 pixels wide 1080 pixels tall) along with your stream output</li>
+                    <li>
+                        The controller page we added initially is the scorebug theme selection page. To begin a stream, do the following:
+                        <ol>
+                            <li>Select a scorebug theme from the selection page. This will open the actual controller page</li>
+                            <li>Refresh the browser source in the stream sources panel</li>
+                            <li> Upon refresh, you should now see the singles version of the scorebug theme appear you are now ready to to use your overlay</li>
+                        </ol>
+                    </li>
+                </ul>
             </div>
         </>
     );
