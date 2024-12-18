c26b9e24e91876332550bc7e0bdc017098d84489
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2024-12-17 23:33:10 -0800

    filling in content on how to use pages

--- src/Components/Tutorials/obschange.jsx
+++ src/Components/Tutorials/obschange.jsx
@@ -9,10 +9,11 @@ const OBSchange = () => {
                 <h3>
                     Changing the theme
                 </h3>
+                <iframe width="600" height="330" src="https://www.youtube.com/embed/BDE7UzaHht8?si=frPmyz2eiOc8QNuU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 <ol>
-                    <li>From the controler page, click the button "Change Theme", this will return you to the scorebug Selection page.</li>
-                    <li>select a new theme</li>
-                    <li>refresh the browser source page in the sources panel of OBS, the new scorebug theme should appear</li>
+                    <li>From the controler page, click the button "Change Theme", this will return you to the Theme Selection page.</li>
+                    <li>select/click on a new theme</li>
+                    <li>refresh the browser source for the scorebug in the sources panel of OBS, the new scorebug theme should appear upon refresh</li>
                 </ol>
             </div>
         </>
--- src/Components/Tutorials/sggAPI.jsx
+++ src/Components/Tutorials/sggAPI.jsx
@@ -7,16 +7,31 @@ const SggAPI = () => {
         <>
             <div className="initialcontain">
                 <h3>
-                    Ussing the start.gg API
+                    Using the start.gg API
                 </h3>
                 <p>Pre-Requisite: Your start.gg account must have admin/moderator access to the tournament page</p>
+                <h4>
+                    On Start.gg:
+                </h4>
+                <ol>
+                    <li></li>
+                </ol>
+                <h4>
+                    On the Simple Smash Scorebugs Controller page:
+                </h4>
                 <ol>
                     <li>From the Score controller page, Click the "Login with Start.gg" button</li>
                     <li>sign into your start.gg account</li>
                     <li>Enter the tournament Slug. this can be found in the tournament URL as follows: https://www.start.gg/tournament/ <b>----SLUG----</b> /events</li>
-                    <li>Enter the username of the twitch/youtube channel for your stream</li>
-                    <li>Now you can click "Fetch From Stream Queue" to get tags from start.gg. Now all thats left is to ensure an tournament moderator adds the desired matchups to the stream queue on start.gg</li>
+                    <li>Enter the username of the twitch/youtube channel for your stream into the streamer name field</li>
+                    <li>Now you can click "Fetch From Stream Queue" and the scorebug will auto populate with the tags and info for the match at the front of the stream Queue</li>
                 </ol>
+                <h4>Final Check/Notice</h4>
+                <p>Once added your work is not yet over.
+                    You will need to ensure that your TO is adding matches to the stream Queue associated with your stream (start.gg supports multiple stream queues for events).
+                    Additionally, matches are also not removed from the queue unless an admin either manually removes it or until a score for a match is entered into start.gg.
+                    To ensure proper function please make sure that your Tournament Organizer promptly enters the scores of stream matches into start.gg.
+                </p>
             </div>
         </>
     );
