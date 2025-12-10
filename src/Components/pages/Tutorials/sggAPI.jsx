d2eef3a522503896ceb9bb959a932ff244b21de8
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2025-12-09 23:37:06 -0800

    tutorial updates, text should be complete

--- src/Components/pages/Tutorials/sggAPI.jsx
+++ src/Components/pages/Tutorials/sggAPI.jsx
@@ -11,27 +11,36 @@ const SggAPI = () => {
                 </h3>
                 <p>Pre-Requisite: Your start.gg account must have admin/moderator access to the tournament page</p>
                 <h4>
-                    On Start.gg:
+                    Start.gg setup:
                 </h4>
                 <ol>
-                    <li></li>
+                    <li>Open the tournament page on start.gg; ensure you are signed in with an account with admin access to the event</li>
+                    <li>Navigate to, Settings then Stations & Streams</li>
+                    <li>Click Add Stream</li>
+                    <li>Fill in the fields. Keep a Note of the Channel Name as that will need to be entered into the scorebug later</li>
+                    <li>Navigate to the bracket you are broadcasting</li>
+                    <li>click on a match</li>
+                    <li>click "stream"</li>
+                    <li>click the username of the channel you are broadcasting on</li>
+                    <li>Repeat steps 6-8 to add matches you plan on broadcasting</li>
                 </ol>
                 <h4>
-                    On the Simple Smash Scorebugs Controller page:
+                    Scorebug Controller Setup (aka this website):
                 </h4>
                 <ol>
                     <li>From the Score controller page, Click the "Login with Start.gg" button</li>
                     <li>sign into your start.gg account</li>
                     <li>Enter the tournament Slug. this can be found in the tournament URL as follows: https://www.start.gg/tournament/ <b>----SLUG----</b> /events</li>
-                    <li>Enter the username of the twitch/youtube channel for your stream into the streamer name field</li>
-                    <li>Now you can click "Fetch From Stream Queue" and the scorebug will auto populate with the tags and info for the match at the front of the stream Queue</li>
+                    <li>In the Streamer Name field, Enter the twitch/youtube channel name</li>
+                    <li>Click the "Fetch From Stream Queue" button. This will auto populate the bug for the match at the front of the queue
+                        <ul>
+                            <li>Between matches, you will always need to click the "Fetch From Stream Queue" button to cycle to the next match</li>
+                            <li>Note that the next match will not be moved to the front of the queue by Start.gg until the current match is marked as complete</li>
+                        </ul>
+                    </li>
                 </ol>
-                <h4>Final Check/Notice</h4>
-                <p>Once added your work is not yet over.
-                    You will need to ensure that your TO is adding matches to the stream Queue associated with your stream (start.gg supports multiple stream queues for events).
-                    Additionally, matches are also not removed from the queue unless an admin either manually removes it or until a score for a match is entered into start.gg.
-                    To ensure proper function please make sure that your Tournament Organizer promptly enters the scores of stream matches into start.gg.
-                </p>
+                <h4>Additional Notes</h4>
+                <p>Using the Stream queue requires proactive attention throughout your event. Make sure you add matches as needed to the queue on start.gg. Keep in mind that this portion requires admin access to start.gg so work closely with your Tournament Organizer (TO) if you dont have this access</p>
             </div>
         </>
     );
